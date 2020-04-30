7060a67b8d36bab652b09906c988a8f43fabd30e
xavigh <fgargallo@gmail.com>
2020-04-30 16:36:06 +0200

    simple http request api example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
@@ -17,10 +18,15 @@ import { ROUTES } from "./app.routes";
     HomeComponent,
     SearchComponent,
     ArtistComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(ROUTES, { useHash: true })],
+
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styles: [],
 })
 export class HomeComponent implements OnInit {
+  countries: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    console.log("this is a http request");
+    this.http
+      .get("https://restcountries.eu/rest/v2/lang/es")
+      .subscribe((resp: any) => {
+        this.countries = resp;
+        console.log(resp);
+      });
   }
 
+  ngOnInit() {}
 }
